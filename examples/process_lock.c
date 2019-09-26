dfac93ffaf22a8c4b8d9cca64ff29f8ef74c5ee6
Derric <Derric_Lynns@comcast.com>
2019-09-26 18:45:34 +0530

    :sparkle: added sample code using lock_proc.h

---
+++ examples/process_lock.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+#define PROC_USE_SEMAPHORE
+#define PROC_USE_LOGGER printf
+
+#include "../include/proc_lock.h"
+
+#ifdef PROC_AUTO
+PROC_AUTO
+#endif
+
+
+int main()
+{
+    //lock
+    PROC_LOCK
+    printf("running speedtest unit test 1.0 \n");
+    sleep(10);
+    printf("sleep is over for speedtest unit test 1.0 \n");
+
+}
+
